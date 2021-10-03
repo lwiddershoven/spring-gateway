1a404feec04c29dc4fcf11fb642a0c65b386935e
lwiddershoven <github@qaz.dds.nl>
2021-10-03 16:49:44 +0200

    Partner service. Like everything else a test application.properties with config and vault properties was added to enable the build

---
+++ partner-service/src/test/kotlin/nl/leonw/partnerservice/PartnerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.leonw.partnerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PartnerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
