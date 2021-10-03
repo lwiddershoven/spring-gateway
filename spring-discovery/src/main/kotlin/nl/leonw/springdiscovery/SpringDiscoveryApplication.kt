444da83e883771fa7672adc5267be68032044a53
lwiddershoven <github@qaz.dds.nl>
2021-10-03 16:34:52 +0200

    Eureka discovery application. Added test application.properties to make the build work with the included config server and vault dependencies

---
+++ spring-discovery/src/main/kotlin/nl/leonw/springdiscovery/SpringDiscoveryApplication.kt
@@ -0,0 +1,11 @@
+package nl.leonw.springdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDiscoveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDiscoveryApplication>(*args)
+}
---
+++ spring-discovery/src/test/kotlin/nl/leonw/springdiscovery/SpringDiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.leonw.springdiscovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDiscoveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
