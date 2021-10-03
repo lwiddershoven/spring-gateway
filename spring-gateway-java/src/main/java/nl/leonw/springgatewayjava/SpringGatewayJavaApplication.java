8ac5937871afcba5be7cf7517365f685292f31eb
lwiddershoven <github@qaz.dds.nl>
2021-10-03 13:45:56 +0200

    Project from start.spring.io, plus test application.properties to make actually make it compile

---
+++ spring-gateway-java/src/main/java/nl/leonw/springgatewayjava/SpringGatewayJavaApplication.java
@@ -0,0 +1,13 @@
+package nl.leonw.springgatewayjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayJavaApplication.class, args);
+	}
+
+}
---
+++ spring-gateway-java/src/test/java/nl/leonw/springgatewayjava/SpringGatewayJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.leonw.springgatewayjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
