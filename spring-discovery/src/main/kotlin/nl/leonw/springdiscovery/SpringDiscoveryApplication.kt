a83fe132e030f4937c5786684839f1b020d428ae
lwiddershoven <github@qaz.dds.nl>
2021-10-04 15:14:16 +0200

    springdiscovery: configured consul and vault based configuration management

--- spring-discovery/src/main/kotlin/nl/leonw/springdiscovery/SpringDiscoveryApplication.kt
+++ spring-discovery/src/main/kotlin/nl/leonw/springdiscovery/SpringDiscoveryApplication.kt
@@ -1,8 +1,14 @@
 package nl.leonw.springdiscovery
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,3 +17,17 @@ class SpringDiscoveryApplication
 fun main(args: Array<String>) {
 	runApplication<SpringDiscoveryApplication>(*args)
 }
+
+@Component
+class ShowAll(val config: Config) : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		println("Config $config")
+	}
+
+}
+
+@Configuration
+@ConfigurationProperties(prefix = "config")
+// Need to be _var_, not val! And have a initial value.
+data class Config(var username: String = "", var password: String = "") {}
+
