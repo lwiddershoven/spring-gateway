95a5f133e13ab4062f87abea9dfa9e945064e7fb
lwiddershoven <github@qaz.dds.nl>
2021-10-04 11:57:32 +0200

    springdiscovery: added EnableEurekaServer and configured properties

--- spring-discovery/src/main/kotlin/nl/leonw/springdiscovery/SpringDiscoveryApplication.kt
+++ spring-discovery/src/main/kotlin/nl/leonw/springdiscovery/SpringDiscoveryApplication.kt
@@ -2,8 +2,10 @@ package nl.leonw.springdiscovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class SpringDiscoveryApplication
 
 fun main(args: Array<String>) {
