fce82850fc8f20ffa645dc51ca7276f14fafca1f
lwiddershoven <github@qaz.dds.nl>
2021-10-09 11:03:12 +0200

    start-infra.sh : springdiscovery (eureka) service discovery service added to start script.

--- start-infra.sh
+++ start-infra.sh
@@ -3,9 +3,16 @@
 echo "Starting infra services using docker"
 echo "This may fail if you have not build the springdiscovery container"
 
+# Because consul and vault are stateless we do not auto restart them, as we
+# don't want to lose state.
+
+
 echo "Start configuration management app 'consul' in development mode"
 docker run -d -p 8500:8500 --name=consul consul
 
 echo "Starting secret management app 'vault' in development mode"
 # The root token is not normally set this way in a prod env. And is usually a lot more secure than 'topsecret'
-docker run --cap-add=IPC_LOCK -d -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=topsecret' --name=vault vault
+docker run -d --cap-add=IPC_LOCK -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=topsecret' --name=vault vault
+
+echo "Starting service discovery service"
+docker run -d -p 8761:8761 library/spring-discovery:0.0.1-SNAPSHOT
